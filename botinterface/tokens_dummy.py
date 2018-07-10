cc8ee6e5f02dfa4eaea71813f664130a9153c140
Kurt-F <truksaver@gmail.com>
2018-07-09 22:25:12 -0400

    Added a token file example

---
+++ botinterface/tokens_dummy.py
@@ -0,0 +1,8 @@
+# Keys and tokens are kept here so that they can be kept out of source control
+# This is a dummy file to show you the structure that is expected
+
+# The telegram bot key
+BOT_KEY = "Your key goes here"
+
+# The token used in the webhook sent to telegram
+WEB_TOKEN = "Your web token goes here"
